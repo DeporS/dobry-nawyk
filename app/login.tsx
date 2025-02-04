9919a0509035644faefc28e162533b4230c2f5db
DeporS <depores1337@gmail.com>
2025-02-04 03:24:05 +0100

    start to add firebase

---
+++ app/login.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "@/components/Themed";
+import React from "react";
+
+export default function LoginScreen() {
+  return (
+    <View>
+      <Text>Login Screen</Text>
+    </View>
+  );
+}
