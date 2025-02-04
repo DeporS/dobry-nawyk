857abfc77c22e9cccf6abed39e8eb94e06f84a3c
DeporS <depores1337@gmail.com>
2025-02-04 15:05:20 +0100

    add working login with firebase

--- app/login.tsx
+++ app/login.tsx
@@ -1,10 +1,55 @@
 import { Text, View } from "@/components/Themed";
-import React from "react";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { auth } from "../FirebaseConfig";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { router } from "expo-router";
+import { Button, TextInput } from "react-native";
 
 export default function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(auth, email, password);
+      if (user) router.replace("/(tabs)");
+    } catch (error: any) {
+      console.log(error);
+      alert("Sign in failed: " + error.message);
+    }
+  };
+
+  const signUp = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(auth, email, password);
+      if (user) router.replace("/(tabs)");
+    } catch (error: any) {
+      console.log(error);
+      alert("Sign in failed: " + error.message);
+    }
+  };
+
   return (
-    <View>
-      <Text>Login Screen</Text>
-    </View>
+    <SafeAreaView>
+      <Text>Login</Text>
+      <TextInput
+        placeholder="email"
+        value={email}
+        onChangeText={setEmail}
+      ></TextInput>
+      <TextInput
+        placeholder="password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Sign In" onPress={signIn} />
+      <Button title="Sign Up" onPress={signUp} />
+    </SafeAreaView>
   );
 }
