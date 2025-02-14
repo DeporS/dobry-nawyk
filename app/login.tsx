cdf62cd687aee882a7f2b7bc1fa48c1e90871316
DeporS <depores1337@gmail.com>
2025-02-14 13:42:02 +0100

    work on login screen

--- app/login.tsx
+++ app/login.tsx
@@ -1,6 +1,7 @@
 import { Text, View } from "@/components/Themed";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, Dimensions } from "react-native";
 import { auth } from "../FirebaseConfig";
 import {
   signInWithEmailAndPassword,
@@ -9,6 +10,7 @@ import {
 } from "firebase/auth";
 import { router } from "expo-router";
 import { Button, TextInput } from "react-native";
+import { red } from "react-native-reanimated/lib/typescript/Colors";
 
 export default function LoginScreen() {
   const [email, setEmail] = useState("");
@@ -35,21 +37,55 @@ export default function LoginScreen() {
   };
 
   return (
-    <SafeAreaView>
-      <Text>Login</Text>
-      <TextInput
-        placeholder="email"
-        value={email}
-        onChangeText={setEmail}
-      ></TextInput>
-      <TextInput
-        placeholder="password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <Button title="Sign In" onPress={signIn} />
-      <Button title="Sign Up" onPress={signUp} />
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Login</Text>
+        <View style={styles.credentialsBox}>
+          <TextInput
+            style={styles.input}
+            placeholder="email"
+            placeholderTextColor="#888"
+            value={email}
+            onChangeText={setEmail}
+          ></TextInput>
+          <TextInput
+            style={styles.input}
+            placeholder="password"
+            placeholderTextColor="#888"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+          />
+        </View>
+        <Button title="Sign In" onPress={signIn} />
+        <Button title="Sign Up" onPress={signUp} />
+      </View>
     </SafeAreaView>
   );
 }
+
+const screenHeight = Dimensions.get("window").height;
+const screenWidth = Dimensions.get("window").width;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: "5%",
+  },
+  credentialsBox: {
+    gap: 10,
+    marginBottom: "5%",
+  },
+  input: {
+    backgroundColor: "white",
+    width: screenWidth * 0.6,
+    height: screenHeight * 0.055,
+    borderRadius: 10,
+    textAlign: "center",
+  },
+});
